9ceb0b0800cc206995d02ddfeb4c810f12a0eebf
Miguelito2926 <juniorprol@hotmail.com>
2025-06-27 19:40:36 -0300

    Adicionando BcryptPassword para criptografar senhas

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$XYGLdD88g1SaeiF7EsGO1uosXpElQEwF2bOIVp7zZ4/CprS7XZMfy');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$XYGLdD88g1SaeiF7EsGO1uosXpElQEwF2bOIVp7zZ4/CprS7XZMfy');
 
 INSERT INTO tb_product (name) VALUES ('TV');
 INSERT INTO tb_product (name) VALUES ('Computer');
